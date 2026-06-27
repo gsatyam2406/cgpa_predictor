ef11223a2c8b2c05668d4560fadaf133a3bfbacc
Satyam Gupta <gsatyam2406@gmail.com>
2026-06-27 12:45:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,37 @@
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
-import joblib
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-
+import joblib
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-model = joblib.load("cgpa_model.pkl")
+def train_model():
+    df = pd.read_csv("student_cgpa_dataset.csv")
+    X = df[['prev_cgpa','attendance_percent','assignments_completed',
+            'midterm_score','lab_score','extracurricular_activities']]
+    y = df['predicted_cgpa']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+    joblib.dump(model, "cgpa_model.pkl")
+    return model
 
+try:
+    model = joblib.load("cgpa_model.pkl")
+except:
+    model = train_model()
 
 class Student(BaseModel):
-
     prev_cgpa: float
     attendance_percent: float
     assignments_completed: float
@@ -25,14 +39,13 @@ class Student(BaseModel):
     lab_score: float
     extracurricular_activities: float
 
-
+@app.get("/")
+def home():
+    return {"message": "CGPA Predictor API running"}
 
 @app.post("/predict")
-
-def predict(student:Student):
-
-
-    data=[[
+def predict(student: Student):
+    data = [[
         student.prev_cgpa,
         student.attendance_percent,
         student.assignments_completed,
@@ -40,14 +53,5 @@ def predict(student:Student):
         student.lab_score,
         student.extracurricular_activities
     ]]
-
-
-    prediction=model.predict(data)
-
-
-    return {
-
-        "predicted_cgpa":
-        round(float(prediction[0]),2)
-
-    }
+    prediction = model.predict(data)
+    return {"predicted_cgpa": round(float(prediction[0]), 2)}
